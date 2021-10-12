0c23138315568643dda3053eb05e96f7ec6faa3e
Gerold Jacqualine <gjacqual@student.21-school.ru>
2021-10-12 19:52:32 +0300

    update lib to norm 3.3.40

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gjacqual <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gjacqual <gjacqual@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 13:28:27 by gjacqual          #+#    #+#             */
-/*   Updated: 2021/05/12 14:10:20 by gjacqual         ###   ########.fr       */
+/*   Updated: 2021/10/12 19:51:40 by gjacqual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 char	*ft_strchr(const char *s, int c)
 {
 	size_t	i;
