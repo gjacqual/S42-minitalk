da171854e6d4a5f8225218b62d5afa0b680060ea
Gerold Jacqualine <gjacqual@student.21-school.ru>
2021-10-14 20:27:01 +0300

    bonus-part update

--- minitalk_bonus/srcs/client_bonus.c
+++ minitalk_bonus/srcs/client_bonus.c
@@ -6,13 +6,13 @@
 /*   By: gjacqual <gjacqual@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:57:46 by gjacqual          #+#    #+#             */
-/*   Updated: 2021/10/14 17:33:02 by gjacqual         ###   ########.fr       */
+/*   Updated: 2021/10/14 20:25:52 by gjacqual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk_bonus.h"
 
-static int	check_real_pid(int serv_pid)
+int	check_real_pid(int serv_pid)
 {
 	if (99 < serv_pid && serv_pid < 99999)
 		return (1);
@@ -23,30 +23,27 @@ static int	check_real_pid(int serv_pid)
 int	ft_bit_decoder(int symbol, int serv_pid)
 {	
 	int	count;
+	int	sig_answer;
 
 	count = 0;
 	while (count < 8)
 	{
+		g_sigrecived = 0;
 		if ((symbol >> count) & 1)
 		{
-			if (kill(serv_pid, SIGUSR1) != 0)
-			{
-				ft_putstr_fd("Error: Serv PID is incorrect!\n", 1);
-				return (0);
-			}
+			sig_answer = kill(serv_pid, SIGUSR1);
 		}
 		else
 		{
-			if (kill(serv_pid, SIGUSR2) != 0)
-			{
-				ft_putstr_fd("Error: Serv PID is incorrect!\n", 1);
-				return (0);
-			}
-		}	
+			sig_answer = kill(serv_pid, SIGUSR2);
+		}
+		if (sig_answer != 0)
+			return (sig_answer);
 		count++;
-		usleep(100);
+		while (!g_sigrecived)
+			continue ;
 	}
-	return (1);
+	return (sig_answer);
 }
 
 int	bit_sender(char *message, int serv_pid)
@@ -56,8 +53,11 @@ int	bit_sender(char *message, int serv_pid)
 	i = 0;
 	while (message[i] != '\0')
 	{
-		if (ft_bit_decoder(message[i], serv_pid) != 1)
+		if (ft_bit_decoder(message[i], serv_pid) != 0)
+		{
+			ft_putstr_fd("Error: Serv PID is incorrect!\n", 1);
 			return (0);
+		}
 		i++;
 	}
 	if (ft_bit_decoder(0, serv_pid) != 1)
@@ -71,11 +71,12 @@ void	ft_server_signal_handler(int sig_nb, siginfo_t *sig_info, void *context)
 	(void)context;
 	if (sig_nb == SIGUSR1)
 	{
-		ft_putstr_fd("1 Symbol received!\n", 1);
+		ft_putstr_fd("Message was received!\n", 1);
+		exit(0);
 	}
 	if (sig_nb == SIGUSR2)
 	{	
-		ft_putstr_fd("Signal received!\n", 1);
+		g_sigrecived = 1;
 	}
 }
 
